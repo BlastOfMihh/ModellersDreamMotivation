b14c523e76eb65f35b12e49045de05be04513dac
LauraC360 <laurachiriac.cl@gmail.com>
2024-06-18 01:57:39 +0300

    home-page

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -4,15 +4,17 @@ import { EngineComponent } from './engine/engine.component';
 import { UiComponent } from './ui/ui.component';
 import { ViewModelComponent } from './viewmodel/engine.component';
 import { NgFor } from '@angular/common';
-import {HomePageComponent} from "./home-page/home-page.component";
+//import {HomePageComponent} from "./home-page/home-page.component";
 import { RouterOutlet } from '@angular/router';
+import { HomePageModule } from './home-page/home-page.module';
 
 @Component({
   imports: [
     ViewModelComponent,
     UiComponent,
     EngineComponent,
-    HomePageComponent,
+    //HomePageComponent,
+    HomePageModule,
     RouterOutlet
   ],
   selector: 'app-root',
--- frontend/src/app/home-page/home-page.component.ts
+++ frontend/src/app/home-page/home-page.component.ts
@@ -1,14 +1,22 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home-page',
-  standalone: true,
-  imports: [],
+  // standalone: true,
+  // imports: [],
   templateUrl: './home-page.component.html',
-  styleUrl: './home-page.component.scss'
+  styleUrls: ['./home-page.component.scss']
   // encapsulation: ViewEncapsulation.None,
   // changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class HomePageComponent {
+export class HomePageComponent implements OnInit {
+  currentContests = ['Current Contest 1', 'Current Contest 2', 'Current Contest 3'];
+  archivedContests = ['Archived Contest 1', 'Archived Contest 2', 'Archived Contest 3'];
+  
 
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 }
